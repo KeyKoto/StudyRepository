7ebf1c3fe924a94d8a90474fd92ca4e3572c27b7
KeyKoto <vav11958@gmail.com>
2024-11-15 19:31:01 +0300

    Task 5 from https://code.mu/ru/python/tasker/stager/1/1/

---
+++ Task5.py
@@ -0,0 +1,7 @@
+#Задание из задачника номер 5. https://code.mu/ru/python/tasker/stager/1/1/
+str1 = str(input("Input a random word: "))
+str2 = str(input("Input an other random world: "))
+if str1[1] == str2[1]:
+    print("First letters of the words are the same!")
+else:
+    print("First letters of the words are different.")
