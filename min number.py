bcb9b86bcae9080f8c041a596fc93c0a6dfef61d
KeyKoto <vav11958@gmail.com>
2024-11-18 21:02:30 +0300

    Нахождение минимального числа в рандомном массиве.

--- min number.py
+++ min number.py
@@ -2,18 +2,17 @@
 import random
 
 array_size = random.randint(5,10)
-print(array_size)
+print("Длина массива: ", array_size)
 
-random_array = [random.randint(1, 100) for i in range(array_size)]
-print(random_array)
+random_array = [random.randint(-100, 100) for i in range(array_size)]
+print("Массив: ", random_array)
 
 min = random_array[0]
-array_size -= 1
 
-for i in range(array_size):
-    if random_array[i] > random_array[i+1]:
-        min = random_array[i+1]
+for i in random_array:
+    if min > i:
+        min = i
     else:
         continue
 
-print(min)
+print("Минимальное число массива: ", min)
