7e6b4a22abb0169c0d92c0c6224e81106f218cce
KeyKoto <vav11958@gmail.com>
2024-11-20 17:49:35 +0300

    Сортировка выбором.

--- Сортировка_выбором.py
+++ Сортировка_выбором.py
@@ -7,28 +7,22 @@ print(len)
 print(array)
 
 intForChanging = 0
-max = array[0]
-id_max = 0
+
 len = len - 1
 i = len
 counter = 0
 
 while len > 0:
+    max = array[0]
+    id_max = 0
     while i > 0:
         if max < array[i]:
             max = array[i]
-            counter = i
+            id_max = i
         i -= 1
-        if counter > 0:
-            id_max = counter
-        else:
-            id_max = 0
     intForChanging = array[len]
     array[len] = max
     array[id_max] = intForChanging
     len -= 1
-    len -= 1
-    max = array[0]
-    counter = 0
     i = len
 print(array)
