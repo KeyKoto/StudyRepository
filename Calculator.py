25d28df3f6e5668fc3b4b134e59bbabdcf31e6a1
KeyKoto <vav11958@gmail.com>
2024-11-22 20:22:05 +0300

    Calculator that made with functions.

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,18 @@
-def calculator(operation):
+def calculator(a, b, operation):
+    res = 0
     if operation == 1:
-        return lambda a, b: a + b
+            res = a + b
     elif operation == 2:
-        return lambda a, b: a - b
+            res = a - b
     elif operation == 3:
-        return lambda a, b: a * b
+            res = a * b
     elif operation == 4:
-        return lambda a, b: a / b
-
+            res = a / b
+    return res
 
 dejstvie = int(input("Выберите операцию: 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление: "))
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
 
-result = calculator(dejstvie)
-result_client = result(num1, num2)
-print(result_client)
+print(calculator(a=num1, b=num2, operation=dejstvie))
+
