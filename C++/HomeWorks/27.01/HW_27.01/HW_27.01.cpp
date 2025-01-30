91c63902bbb154f252a331c3bee0b3dc55704926
KeyKoto <vav11958@gmail.com>
2025-01-30 18:57:23 +0300

    Загрузила окончательную дз за 27.01

--- C++/HomeWorks/27.01/HW_27.01/HW_27.01.cpp
+++ C++/HomeWorks/27.01/HW_27.01/HW_27.01.cpp
@@ -3,8 +3,7 @@
 #include <iostream>
 using namespace std;
 
-int maxMas()
-{
+int maxMas() {
     /*Задача 1: Найти максимальный элемент массива
        1. Создать массив из 10 чисел(заполнить вручную или с помощью rand()).
        2. Найти максимальный элемент массива с помощью цикла for.
@@ -15,28 +14,24 @@ int maxMas()
        Максимальный элемент : 9
    */
 
-    int mas[10]{};
+    int mas[10] {};
 
-    for (int i = 0; i < size(mas); i++)
-    {
+    for (int i = 0; i < size(mas); i++) {
         cin >> mas[i];
     }
 
     cout << "Массив: ";
 
-    for (int i = 0; i < size(mas); i++)
-    {
+    for (int i = 0; i < size(mas); i++) {
         cout << mas[i] << ' ';
     }
 
-    int max = 0;
+    int max {0};
 
-    for (int i = 0; i < size(mas); i++)
-    {
+    for (int i = 0; i < size(mas); i++) {
         if (mas[i] > max)
-        {
             max = mas[i];
-        }
+        
     }
 
     cout << '\n' << "Максимальное значение: " << max;
@@ -44,8 +39,7 @@ int maxMas()
     return 0;
 }
 
-int sum()
-{
+int sum() {
     /*Задача 2: Сумма элементов на четных позициях
 1. Найти сумму элементов массива на четных позициях(индексы: 0, 2, 4...).
 2. Использовать цикл for для подсчета суммы.
@@ -55,26 +49,22 @@ int sum()
 Массив: 3, 8, 1, 5, 9, 4, 7, 6, 2, 0
 Сумма элементов на четных позициях : 22*/
 
-    int index = 0;
-    int sum = 0;
-    int mas[10]{};
-    int output[5]{};
-    for (int i = 0; i < size(mas); i++)
-    {
+    int index {0};
+    int sum {0};
+    int mas[10] {};
+    int output[5] {};
+    for (int i = 0; i < size(mas); i++) {
         cin >> mas[i];
     }
 
     cout << "Массив: " << endl;
 
-    for (int i = 0; i < size(mas); i++)
-    {
+    for (int i = 0; i < size(mas); i++) {
         cout << mas[i] << ' ';
     }
 
-    for (int i = 0; i < size(mas); i++)
-    {
-        if (i % 2 == 0)
-        {
+    for (int i = 0; i < size(mas); i++) {
+        if (i % 2 == 0) {
             output[index] = mas[i];
             sum += mas[i];
             index++;
@@ -83,8 +73,7 @@ int sum()
 
     cout << "Элементы на четных позициях: ";
 
-    for (int i = 0; i < size(output); i++)
-    {
+    for (int i = 0; i < size(output); i++) {
         cout << output[i] << ' ';
     }
 
@@ -95,51 +84,55 @@ int sum()
 
 }
 
-int main()
-{
-    setlocale(LC_ALL, "Russian");
-
+int mas() {
     /*Задача 3: Разворот массива
         1. Создать новый массив, в который записать элементы исходного массива в обратном порядке.
         2. Использовать цикл for для копирования.
         3. Вывести оригинальный и развернутый массив.
 
         Пример вывода :
-    Оригинальный массив : 3, 8, 1, 5, 9, 4, 7, 6, 2, 0
+        Оригинальный массив : 3, 8, 1, 5, 9, 4, 7, 6, 2, 0
         Развернутый массив : 0, 2, 6, 7, 4, 9, 5, 1, 8, 3*/
 
     int mas[10]{};
     int arr[10]{};
 
-    for (int i = 0; i < size(mas); i++)
-    {
+
+    for (int i = 0; i < size(mas); i++) {
         cin >> mas[i];
     }
 
     cout << "Оригинальный массив: ";
 
-    for (int i = 0; i < size(mas); i++)
-    {
+    for (int i = 0; i < size(mas); i++) {
         cout << mas[i] << ' ';
     }
 
     cout << endl;
 
-    for (int i = size(mas) - 1; i > 0; i--)
-    {
-        for (int j = 0; j < size(arr); j++)
-        {
-            arr[j] = mas[i];
-        }
+    for (int i = size(mas) - 1; i >= 0; i--) {
+        int counter{ 0 };
+        arr[counter] = mas[i];
+        counter++;
     }
 
     cout << "Перевернутый массив: ";
 
-    for (int i = 0; i < size(arr); i++)
-    {
+    for (int i = 0; i < size(arr); i++) {
         cout << arr[i] << ' ';
     }
 
     cout << endl;
 
+    return 0;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    //maxMas();
+    //sum();
+    //mas()
+
+    return 0;
 }
